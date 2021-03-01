780123c114b250439a2541b4a04d8032c299056e
Tom Leszczysnki <leszczytom@gmail.com>
2021-03-01 17:18:37 +0100

    Methods to access the database added

--- app/src/main/java/fr/uavignon/ceri/tp2/data/BookDao.java
+++ app/src/main/java/fr/uavignon/ceri/tp2/data/BookDao.java
@@ -1,11 +1,23 @@
 package fr.uavignon.ceri.tp2.data;
 
 import java.util.List;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.RawQuery;
+import androidx.room.Update;
 
+@Dao
 public interface BookDao {
+    @Update
     void updateBook(Book book);
+    @Insert
     void insertBook(Book book);
+    //Todo: query qqch
     Book getBook(long id);
+    @Delete
     void deleteBook(long id);
+    @Query("Select * from Book")
     List<Book> getAllBooks();
 }
