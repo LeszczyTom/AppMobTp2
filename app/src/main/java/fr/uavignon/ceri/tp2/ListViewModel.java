f439d32c0ddd9f2dd2f836046037ca886331ff1a
Tom Leszczysnki <leszczytom@gmail.com>
2021-03-03 18:25:59 +0100

    ListViewModel added

---
+++ app/src/main/java/fr/uavignon/ceri/tp2/ListViewModel.java
@@ -0,0 +1,23 @@
+package fr.uavignon.ceri.tp2;
+
+import android.app.Application;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import fr.uavignon.ceri.tp2.data.Book;
+import fr.uavignon.ceri.tp2.data.BookRepository;
+
+public class ListViewModel extends AndroidViewModel {
+    private BookRepository repo;
+    private LiveData<List<Book>> allProducts;
+
+    public ListViewModel(@NonNull Application application) {
+        super(application);
+        repo = new BookRepository(application);
+        allProducts = repo.getAllBooks();
+    }
+}
